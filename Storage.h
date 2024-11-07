33c0f3adcf97de0b29fac1faec70065a101ee1f2
gleblor <loreshgleb523@gmail.com>
2024-11-07 19:59:31 +0300

    Add files via upload

---
+++ Storage.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Train.h"
+
+using namespace std;
+
+class Store
+{
+private:
+    Train **array; // массив для хранения значений
+    int countElement; // кол-ва элементов
+
+public:
+    Store(); // конструктор по умолчанию
+    Store(int size); // конструктор с параметром
+    Store(const Store &s); // конструктор копирования
+    ~Store(); // деструктор
+
+    void SortedArray(); // сортировка после ввода
+    void FindTrain(); // поиск по пункту назначения
+    void ExtractField(); // извлечь значения объекта
+    void GetFiel(); // получить поля
+    void Set(); // вставить значение объекта
+    void Change(); // изменить значение
+    void Delete(); // удалить значние
+};
